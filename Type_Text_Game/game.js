c8be4aac49fe436fbfcd7accc990ab91a33237d3
Luiz E. Carneiro <140028787+Luiz-E-Carneiro@users.noreply.github.com>
2024-01-07 22:17:14 -0300

    Update game.js

--- Type_Text_Game/game.js
+++ Type_Text_Game/game.js
@@ -1,30 +1,82 @@
-var keyboard = [
-    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+var finishSound = new Audio('./assets/sounds/finish.mp3');
+var alphabet = [
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '-',
     'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ç',
-    ',','Z', 'X', 'C', 'V', 'B', 'N', 'M', '-', ' '
-    ]
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', ' ',
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '-',
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç',
+    'z', 'x', 'c', 'v', 'b', 'n', 'm'
+];
 
-
-var letters = document.getElementsByClassName('spanLetter')
+var allLetters = document.getElementsByClassName('spanLetter');
 
 const startGame = () => {
-    letters[0].classList.add('selectedLetter')
-}
+    allLetters[0].classList.add('selectedLetter');
+};
 
-var index = 0
 const passLetter = () => {
-    letters[index - 1].classList.remove('selectedLetter')
-    letters[index - 1].classList.add('correctLetter')
-    
-    letters[index].classList.add('selectedLetter')
-}
+    allLetters[index - 1].classList.remove('selectedLetter');
+    allLetters[index - 1].classList.add('correctLetter');
+    if (index <= allLetters.length - 1) allLetters[index].classList.add('selectedLetter');
+};
+
+const wrongLetter = () => {
+    if (index > 0) {
+        allLetters[index - 1].classList.remove('selectedLetter');
+    }
+    allLetters[index].classList.add('wrongLetter');
+    wrong = true;
+};
+
+const goBack = () => {
+    allLetters[index].classList.remove('wrongLetter');
+    wrong = false;
+};
+
+const actionKeyboard = (key) => {
+    const keysDiv = document.getElementsByClassName('keyDiv');
+    for (let i = 0; i < keysDiv.length; i++) {
+        var currentLetter = keysDiv[i].innerText;
+        key = key.trim().toUpperCase();
+        if (key === currentLetter) {
+            keysDiv[i].classList.add('clicked');
+            setTimeout(() => {
+                keysDiv[i].classList.remove('clicked');
+            }, 200);
+        }
+    }
+};
+
+var index = 0;
+var upperCase = false;
+var wrong = false;
 
 document.addEventListener('keydown', function (e) {
-    var key = e.key.toUpperCase()
-
-    if(keyboard.includes(key)){
-        index++
-        passLetter()
+    var key = e.key;
+    if (key === 'CapsLock') upperCase == true ? upperCase = false : upperCase = true;
+    if (alphabet.includes(key) && !wrong) {
+        var keyClick = new Audio('./assets/sounds/click.mp3');
+        keyClick.playbackRate = 2.0
+        keyClick.play();
+        verificLetter(key);
+        actionKeyboard(key);
     }
-    
-})
+    if (key === 'Backspace') goBack();
+});
+
+const verificLetter = (key) => {
+    var cureentLetterText = allLetters[index].innerText;
+    var keyTyped = key;
+    if (allLetters[index].innerHTML == `&nbsp;` && key === " ") {
+        index++;
+        passLetter();
+    } else {
+        if (upperCase) keyTyped = keyTyped.toUpperCase();
+        if (cureentLetterText === keyTyped) {
+            index++;
+            passLetter();
+        } else {
+            wrongLetter();
+        }
+    }
+};
