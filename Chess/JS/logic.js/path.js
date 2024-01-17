9b58e99de031330798520144198a1f6ebd39e08f
Luiz E. Carneiro <140028787+Luiz-E-Carneiro@users.noreply.github.com>
2024-01-16 23:58:45 -0300

    Update path.js

--- Chess/JS/logic.js/path.js
+++ Chess/JS/logic.js/path.js
@@ -1,185 +1,111 @@
 var helpersDots = []
-var possibleMoves = []
-var verificCell
-var lastPiece
-var lastCell
-var lastImg
-let cellsWithPieces = separatePieces(boardObj);
-var pieces = document.getElementsByClassName('piece')
 
-function separatePieces(board) {
-    let allCellsPiece = []
-    let cellsBlackPieces = [];
-    let cellsWhitePieces = [];
-    board.map((line) => {
-        let linePieces = line.filter((cell) => cell.piece);
-        linePieces.map((cell) => cell.piece.color === 'black' ? cellsBlackPieces.push(cell) : cellsWhitePieces.push(cell));
-        linePieces.map((cell) => allCellsPiece.push(cell));
-
+boardObj.forEach(line => {
+    line.forEach(obj => {
+        let cell = obj.cell
+        cell.addEventListener('click', function () {
+            if(!verificCell(obj)){} //celanHelpers() 
+        })
     });
-    return { allCellsPiece, cellsBlackPieces, cellsWhitePieces };
+});
+
+const verificCell = (currentObj) => {
+    let { cell, piece } = currentObj
+
+    let elementosFilhos = cell.children;
+    for (let i = 0; i < elementosFilhos.length; i++) {
+        if (elementosFilhos[i].tagName.toLowerCase() === 'img') {
+            verificPieceToPath(currentObj, piece)
+            return true
+        }else {
+            return false
+        }
+    }
+    
 }
 
-for (let p of pieces) {
-    p.addEventListener('click', () => addHelpPath(p))
-}
-
-const pawnPath = (object) => {
-    const { line, column, piece } = object;
-    var color = piece.color
+const pawnPath = (currentObj) => {
+    const { line, column, color } = currentObj;
     var x
     color === 'white' ? x = 1 : x = -1
 
     var moves = []
     var captures = []
     let inFront = boardObj[line - (1 * x)][column]
-    let nextFront = boardObj[line - (2 * x)][column]
-
-    if (!inFront.piece && nextFront.piece) moves.push(inFront)
-    else moves.push(inFront, nextFront)
-
-    var right = true
-    var left = true
-    if (column === 0 || column === 7) { column === 0 ? left = false : right = false }
-
-    if (!left) {
-        let dgnRight = boardObj[line - (1 * x)][column + 1]
-        if (dgnRight.piece) captures.push(dgnRight)
-    } else if (!right) {
-        let dgnLeft = boardObj[line - (1 * x)][column - 1]
-        if (dgnLeft.piece) captures.push(dgnLeft)
-    } else {
-        let dgnLeft = boardObj[line - (1 * x)][column - 1]
-        let dgnRight = boardObj[line - (1 * x)][column + 1]
-        if (boardObj[line - (1 * x)][column + 1].piece) captures.push(dgnRight)
-        if (boardObj[line - (1 * x)][column - 1].piece) captures.push(dgnLeft)
+    let nextFront
+    if (line != 1 && line != 6) {
+        nextFront = boardObj[line - (2 * x)][column]
     }
-    possibleMoves.push({ moves: moves, captures: captures })
-    paintPath(moves, captures, piece.color)
+    if (!inFront.piece && nextFront.piece) moves.push(inFront)
+    else if (!inFront.piece && !nextFront.piece) moves.push(inFront, nextFront)
+    else return
 
+    let getRight = true;
+    let getLeft = true;
+
+    if (column === 0 || column === 7) {
+        getLeft = column === 0 ? false : getLeft;
+        getRight = column === 7 ? false : getRight;
+    }
+    const checkAndPushCapture = (targetColumn) => {
+        const diagonalCell = boardObj[line - (1 * x)][targetColumn];
+        if (diagonalCell.piece && diagonalCell.piece.color !== color) {
+            captures.push(diagonalCell);
+        }
+    };
+    if (!getLeft) {
+        checkAndPushCapture(column + 1);
+    } else if (!getRight) {
+        checkAndPushCapture(column - 1);
+    } else {
+        checkAndPushCapture(column - 1);
+        checkAndPushCapture(column + 1);
+    }
+    paintPath(moves, captures)
 }
 
-const paintPath = (moves, captures, color) => {
-    var color = color
+const paintPath = (moves, captures) => {
     moves.forEach(c => {
         var help = document.createElement('div')
         helpersDots.push(help)
         c.cell.appendChild(help)
-        if (!c.piece) help.classList.add('ball')
+        help.classList.add('ball')
         c.cell.classList.add('path')
     })
     if (captures.length > 0) {
         captures.forEach(c => {
             var help = document.createElement('div')
             helpersDots.push(help)
-            if (c.piece.color != color) help.classList.add('ring')
+            help.classList.add('ring')
             c.cell.appendChild(help)
             c.cell.classList.add('path')
         });
     }
-    ableToPlay()
 }
 
-const removeHelpers = () => {
-    console.log(helpersDots);
-    helpersDots.forEach(div => {
-        div.parentNode.removeChild(div)
-    });
-    helpersDots = []
-    possibleMoves = []
-    lastPiece = ''
-    lastCell = ''
-    verificCell = ''
-    console.log(helpersDots);
-}
+const verificPieceToPath = (currentObj, piece) => {
+    switch (piece.name) {
+        case 'pawn':
+            pawnPath(currentObj)
+            break;
+        case 'rook':
 
+            break;
+        case 'knight':
 
-const ableToPlay = () => {
-    possibleMoves[0].moves.forEach(c => {
-        c.cell.addEventListener('click', function () {
-            let piece = document.createElement('img')
-            piece.src = lastPiece.src
-            this.appendChild(piece)
-            
-            let lastLine = lastCell.line
-            let lastColumn = lastCell.column
-            let currLine = c.line
-            let currColumn = c.column
-            console.log(boardObj);
-            console.log(boardObj[lastLine][lastColumn]);
-            boardObj[lastLine][lastColumn].piece = false
-            console.log(boardObj[lastLine][lastColumn]);
-            boardObj[currLine][currColumn].piece = { piece: { name: lastPiece.name, color: lastPiece.color, src: `./../assets/images/${lastPiece.name}${lastPiece.color[0].toUpperCase()}.png` } }
-            lastImg.parentNode.removeChild(lastImg)
-            
-            removeHelpers()
-            cellsWithPieces = separatePieces(boardObj);
-            pieces = document.getElementsByClassName('piece')
-        })
-    });
-    // possibleMoves.captures.forEach(cellPiece => {
-    //     cellPiece.addEventListener('click', function () {
+            break;
+        case 'bishop':
 
-    //     })
-    // })
-}
+            break;
+        case 'queen':
 
+            break;
+        case 'king':
 
+            break;
 
-const rookPath = () => {
-    const { line, column } = object;
-
-
-
-}
-const knightPath = () => {
-
-}
-const bishopPath = () => {
-
-}
-const queenPath = () => {
-
-}
-const kingPath = () => {
-
-}
-
-const addHelpPath = (p) => {
-    var parent = p.parentNode
-    cellsWithPieces.allCellsPiece.forEach(c => {
-        if (parent === c.cell) {
-            if (parent != verificCell) {
-                lastImg = p
-                verificCell = c.cell
-                lastCell = { ...c }
-                lastPiece = c.piece
-                switch (c.piece.name) {
-                    case 'pawn':
-                        pawnPath(c)
-                        break;
-                    // case 'rook':
-                    //     rookPath()
-                    //     break;
-                    // case 'knight':
-                    //     knightPath()
-                    //     break;
-                    // case 'bishop':
-                    //     bishopPath()
-                    //     break;
-                    // case 'queen':
-                    //     queenPath()
-                    //     break;
-                    // case 'king':
-                    //     kingPath()
-                    //     break;
-                    // default:
-                    //     alert('error, try again')
-                    //     break;
-                }
-            } else {
-                removeHelpers()
-            }
-        }
-    });
+        default:
+            alert('Something went wrong, try again please!')
+            break;
+    }
 }
