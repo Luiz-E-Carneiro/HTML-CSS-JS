62c5df6b0286cd6de82d675a23129cec794d9cf0
Luiz E. Carneiro <140028787+Luiz-E-Carneiro@users.noreply.github.com>
2024-01-17 21:50:30 -0300

    Update path.js

--- Chess/JS/logic.js/path.js
+++ Chess/JS/logic.js/path.js
@@ -7,16 +7,16 @@ boardObj.forEach(line => {
         let cell = obj.cell
         cell.addEventListener('click', function () {
             if (obj.possibleMove) {
-                movePiece(obj)
                 cleanHelpers()
                 letPlayable(true)
+                movePiece(obj)
             } else {
                 if (currentObj === obj) {
-                    cleanHelpers()
                     currentObj = ''
+                    cleanHelpers()
                 }
                 else {
-                    currentObj = {...obj}
+                    currentObj = obj
                     verificCell(currentObj)
 
                 }
@@ -28,8 +28,7 @@ boardObj.forEach(line => {
 const verificCell = (divObj) => {
     cleanHelpers()
     letPlayable(true)
-    let { cell, piece } = {...divObj} //AQUI N APARECE NADA, ESSE É O ÚNICO PROBLEMA
-    console.log(piece.name);
+    let { cell, piece } = { ...divObj }
     var img = false
 
     let elementosFilhos = cell.children;
@@ -38,11 +37,36 @@ const verificCell = (divObj) => {
             img = true
         }
     }
-    console.log(img);
     if (img) verificPieceToPath(divObj, piece)
 
 }
 
+const verificPieceToPath = (divObj, piece) => {
+    switch (piece.name) {
+        case 'pawn':
+            pawnPath(divObj)
+            break;
+        case 'rook':
+            rookPath(divObj)
+            break;
+        case 'knight':
+            kingPath(divObj)
+            break;
+        case 'bishop':
+            bishopPath(divObj)
+            break;
+        case 'queen':
+            queenPath(divObj)
+            break;
+        case 'king':
+            kingPath(divObj)
+            break;
+
+        default:
+            alert('Something went wrong, try again please!')
+            break;
+    }
+}
 
 const pawnPath = (divObj) => {
     const { line, column, piece } = divObj;
@@ -56,7 +80,6 @@ const pawnPath = (divObj) => {
     let nextFront = boardObj[line - (2 * x)][column]
     if (!inFront.piece && nextFront.piece) moves.push(inFront)
     else if (!inFront.piece && !nextFront.piece) moves.push(inFront, nextFront)
-    else return
 
     let getRight = true;
     let getLeft = true;
@@ -66,9 +89,7 @@ const pawnPath = (divObj) => {
         getRight = column === 7 ? false : getRight;
     }
     const checkAndPushCapture = (targetColumn) => {
-        const diagonalCell = boardObj[line - (1 * x)][targetColumn];
-        console.log(diagonalCell.piece);
-
+        var diagonalCell = boardObj[line - (1 * x)][targetColumn];
         if (diagonalCell.piece && diagonalCell.piece.color != color) {
             captures.push(diagonalCell);
         }
@@ -84,99 +105,18 @@ const pawnPath = (divObj) => {
     paintPath(moves, captures)
 }
 
-const paintPath = (moves, captures) => {
-    moves.forEach(c => {
-        var help = document.createElement('div')
-        helpersDots.push(help)
-        c.cell.appendChild(help)
-        help.classList.add('ball')
-        c.cell.classList.add('path')
-    })
-    if (captures.length > 0) {
-        captures.forEach(c => {
-            var help = document.createElement('div')
-            helpersDots.push(help)
-            help.classList.add('ring')
-            c.cell.appendChild(help)
-            c.cell.classList.add('path')
-        });
-    }
-    possiblePlays.push(moves, captures)
-    letPlayable()
+const rookPath = (divObj) => {
+
 }
+const knightPath = (divObj) => {
 
-const cleanHelpers = () => {
-    helpersDots.forEach(div => {
-        div.parentNode.removeChild(div)
-    });
-    helpersDots = []
 }
+const bishopPath = (divObj) => {
 
-const letPlayable = (exclue = false) => {
-    possiblePlays.forEach(arrayDivs => {
-        arrayDivs.forEach(c => {
-            let { column, line } = c
-            if (exclue) {
-                delete boardObj[line][column].possibleMove
-            } else {
-                let { column, line } = c
-                boardObj[line][column].possibleMove = true
-            }
-        });
-    });
 }
+const kingPath = (divObj) => {
 
-const movePiece = (newSpot) => {
-    //curentObj -- to get out
-    //NewSpot -- to go
-    var img
-    //DELET IMG
-    let elementosFilhos = currentObj.cell.children;
-    for (let i = 0; i < elementosFilhos.length; i++) {
-        if (elementosFilhos[i].tagName.toLowerCase() === 'img') {
-            img = elementosFilhos[i]
-            elementosFilhos[i].parentNode.removeChild(elementosFilhos[i])
-        }
-    }
-    //DELETE piece from the main OBJ
-    const { column, line } = currentObj
-    const { name, color } = currentObj.piece
-    console.log(name);
-    boardObj[line][column].piece = false
-    // ADD IT TO THE NEW CELL
-    newSpot.cell.appendChild(img)
-    // ADD IN THE MAIN OBJ
-    var newColumn = newSpot.column
-    var newLine = newSpot.line
-    var WB = color === 'white' ? 'W' : 'B'
-    boardObj[newLine][newColumn].piece = { piece: { name: name, color: color, src: `./../assets/images/${name}${WB}.png` } }
 }
+const queenPath = (divObj) => {
 
-const verificPieceToPath = (divObj, piece) => {
-
-    console.log(piece.name);
-    switch (piece.name) {
-        case 'pawn':
-            pawnPath(divObj)
-            break;
-        case 'rook':
-
-            break;
-        case 'knight':
-
-            break;
-        case 'bishop':
-
-            break;
-        case 'queen':
-
-            break;
-        case 'king':
-
-            break;
-
-        default:
-            alert('Something went wrong, try again please!')
-            break;
-    }
 }
