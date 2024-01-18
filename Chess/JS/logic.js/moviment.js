ed37251d932b415fddb2627bf958941870d2428e
Luiz E. Carneiro <140028787+Luiz-E-Carneiro@users.noreply.github.com>
2024-01-18 19:57:16 -0300

    General Movement Done

--- Chess/JS/logic.js/moviment.js
+++ Chess/JS/logic.js/moviment.js
@@ -5,9 +5,8 @@ const movePiece = (newSpot) => {
     let capturedWhite = capturedAreas[0]
     let capturedBlack = capturedAreas[1]
     var img
-    var captured
     //DELET IMG    
-    function deletePiece(childrens, newS) {
+    function deletePiece(childrens, newS = false) {
         for (let i = 0; i < childrens.length; i++) {
             if (childrens[i].tagName.toLowerCase() === 'img') {
                 img = childrens[i]
@@ -19,21 +18,29 @@ const movePiece = (newSpot) => {
                     newSpot.piece.color === 'white' ? capturedWhite.appendChild(imgConteiner) : capturedBlack.appendChild(imgConteiner)
 
                 }
-            }  
+            }
         }
     }
     //It has to be in thus order of deletting pieces
     deletePiece(newSpot.cell.children, true)
-    deletePiece(currentObj.cell.children, false)
+    deletePiece(currentObj.cell.children)
     //DELETE piece from the main OBJ
-    const { column, line } = currentObj
+    const { column, line } = {...currentObj}
     const { name, color } = currentObj.piece
-    boardObj[line][column].piece = false
     // ADD IT TO THE NEW CELL
     newSpot.cell.appendChild(img)
     // ADD IN THE MAIN OBJ
     var newColumn = newSpot.column
     var newLine = newSpot.line
     var WB = color === 'white' ? 'W' : 'B'
-    boardObj[newLine][newColumn].piece = { name: name, color: color, src: `./../assets/images/${name}${WB}.png` }
+        // Piece Check
+    if(currentObj.piece.name === 'pawn' && currentObj.piece.firstPlay === false){
+        boardObj[newLine][newColumn].piece = { name: name, color: color, firstPlay:true , src: `./../assets/pieces/${name}${WB}.png` }
+    }else if(currentObj.piece.name === 'pawn'){
+        boardObj[newLine][newColumn].piece = { name: name, color: color, firstPlay: true, src: `./../assets/pieces/${name}${WB}.png` }
+        if(newLine === 0 || newLine === 7 ) promotePawn(boardObj[newLine][newColumn])
+    }else{
+        boardObj[newLine][newColumn].piece = { name: name, color: color, src: `./../assets/pieces/${name}${WB}.png` }
+    }
+    boardObj[line][column].piece = false
 }
