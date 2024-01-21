ba2241c6325558ce28e861d916947ef243b4e1f1
Luiz E. Carneiro <140028787+Luiz-E-Carneiro@users.noreply.github.com>
2024-01-20 22:39:10 -0300

    Update path.js

--- Chess/JS/logic.js/path.js
+++ Chess/JS/logic.js/path.js
@@ -1,5 +1,6 @@
 var player = 'Player1'
 var helpersDots = []
+var possibleCastle = { left: null, right: null }
 var possiblePlays = []
 var currentObj = ''
 
@@ -7,7 +8,12 @@ boardObj.forEach(line => {
     line.forEach(obj => {
         let cell = obj.cell
         cell.addEventListener('click', function () {
-            if (obj.possibleMove) {
+            if (obj.castle) {
+                castle(obj)
+                player === 'Player1' ? player = 'Player2' : player = 'Player1'
+                console.log(boardObj);
+            }
+            else if (obj.possibleMove) {
                 movePiece(obj)
                 refrash()
                 player === 'Player1' ? player = 'Player2' : player = 'Player1'
@@ -52,7 +58,7 @@ const verificPieceToPath = (divObj, piece) => {
             rookPath(divObj)
             break;
         case 'knight':
-            kingPath(divObj)
+            knightPath(divObj)
             break;
         case 'bishop':
             bishopPath(divObj)
@@ -78,6 +84,7 @@ const pawnPath = (divObj) => {
 
     var moves = []
     var captures = []
+
     if (!piece.firstPlay) {
         let inFront = boardObj[line - (1 * x)][column]
         let nextFront = boardObj[line - (2 * x)][column]
@@ -113,17 +120,204 @@ const pawnPath = (divObj) => {
 }
 
 const rookPath = (divObj) => {
+    const { line, column, piece } = divObj;
+    var color = piece.color
+    var moves = []
+    var captures = []
 
+    const cellsToPaint = getVerticalHorizontal(line, column, color, ['left', 'right', 'up', 'down'], moves, captures)
+    paintPath(cellsToPaint[0], cellsToPaint[1])
 }
-const knightPath = (divObj) => {
 
+const knightPath = (divObj, validator = false) => {
+    const { line, column, piece } = divObj;
+    var color = piece.color
+    var moves = []
+    var captures = []
+
+    function addMove(row, col) {
+        var cell = boardObj[row][col]
+        if (cell && !cell.piece) moves.push(cell)
+        else if (cell && cell.piece.color != color) captures.push(cell)
+    }
+
+    if (line + 2 <= 7) {
+        if (column + 1 <= 7) addMove(line + 2, column + 1)
+        if (column - 1 >= 0) addMove(line + 2, column - 1)
+    }
+    if (line - 2 >= 0) {
+        if (column + 1 <= 7) addMove(line - 2, column + 1)
+        if (column - 1 >= 0) addMove(line - 2, column - 1)
+    }
+    if (line + 1 <= 7) {
+        if (column + 1 <= 7) addMove(line + 1, column + 2)
+        if (column - 1 >= 0) addMove(line + 1, column - 2)
+    }
+    if (line - 1 >= 0) {
+        if (column + 1 <= 7) addMove(line - 1, column + 2)
+        if (column - 1 >= 0) addMove(line - 1, column - 2)
+    }
+    paintPath(moves, captures)
 }
+
 const bishopPath = (divObj) => {
+    const { line, column, piece } = divObj;
+    var color = piece.color
+    var moves = []
+    var captures = []
+    const cellsToPaint = getDiagonals(line, column, color, ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'], moves, captures)
+    paintPath(cellsToPaint[0], cellsToPaint[1])
 
 }
+
 const kingPath = (divObj) => {
+    const { line, column, piece } = divObj;
+    var color = piece.color
+    var moves = []
+    var captures = []
+    let directions = [[0, 1], [1, 0], [1, 1]]
 
+    for (let d of directions) {
+        for (let i = 0; i < d.length; i++) {
+            if (line + d[0] <= 7 && column + d[1] <= 7) addMove(line + d[0], column + d[1])
+            if (line + d[0] <= 7 && column - d[1] >= 0) addMove(line + d[0], column - d[1])
+            if (line - d[0] >= 0 && column + d[1] <= 7) addMove(line - d[0], column + d[1])
+            if (line - d[0] >= 0 && column - d[1] >= 0) addMove(line - d[0], column - d[1])
+        }
+    }
+    if (!piece.firstPlay) verificCastle(divObj)
+
+    function addMove(row, col) {
+        var cell = boardObj[row][col]
+        if (cell && !cell.piece) {
+            if (color === 'white' && !cell.whiteKingCannotStay) moves.push(cell)
+            if (color === 'black' && !cell.blackKingCannotStay) moves.push(cell)
+        }
+        else if (cell && cell.piece.color != color && !cell.blackKingCannotStay) captures.push(cell)
+    }
+    paintPath(moves, captures)
 }
-const queenPath = (divObj) => {
 
+const queenPath = (divObj) => {
+    const { line, column, piece } = divObj
+    var color = piece.color
+    var moves = []
+    var captures = []
+    const verticalHorizontal = getVerticalHorizontal(line, column, color, ['left', 'right', 'up', 'down'], moves, captures)
+    const diagonals = getDiagonals(line, column, color, ['topLeft', 'topRight', 'bottomLeft', 'bottomRight'], moves, captures)
+
+    var allMoves = verticalHorizontal[0].concat(diagonals[0])
+    var allCaptures = verticalHorizontal[1].concat(diagonals[1])
+    paintPath(allMoves, allCaptures)
+}
+
+const verificCastle = (divObj) => {
+    const { line, column, piece } = divObj
+    const color = piece.color
+
+    const allPieces = boardObj.map(line =>
+        line.map(cell => cell.piece && cell.piece.name === 'rook' && !cell.piece.firstPlay && cell.piece.color === color ? cell : null)
+    );
+    const filteredRooks = allPieces.flat().filter(cell => cell !== null);
+    if (filteredRooks.length == 0) return
+    filteredRooks.forEach(rookCell => {
+        if (rookCell.column === 0) verificEmptySpaces('left', rookCell.column)
+        if (rookCell.column === 7) verificEmptySpaces('right', rookCell.column)
+    });
+
+    function verificEmptySpaces(side, rookColumn) {
+        var emptySpaces = true
+        if (side === 'left') {
+            for (let i = rookColumn + 1; i < column; i++) {
+                if (boardObj[line][i].piece) emptySpaces = false
+            }
+        } else {
+            for (let i = rookColumn - 1; i > column; i--) {
+                if (boardObj[line][i].piece) emptySpaces = false
+            }
+        }
+
+        if (emptySpaces) {
+            var help = document.createElement('div')
+            helpersDots.push(help)
+            help.classList.add('square')
+            console.log(side);
+            if (side === 'left') {
+                boardObj[line][column - 2].cell.appendChild(help)
+                boardObj[line][column - 2].cell.classList.add('path')
+                boardObj[line][column - 2].castle = true
+                possibleCastle.left = boardObj[line][column - 2].cell
+            } else {
+                boardObj[line][column + 2].cell.appendChild(help)
+                boardObj[line][column + 2].cell.classList.add('path')
+                boardObj[line][column + 2].castle = true
+                possibleCastle.right = boardObj[line][column + 2].cell
+            }
+        }
+        console.log(possibleCastle);
+    }
+}
+const castle = (cellObj) => {
+    var { line, column } = cellObj
+    var color = cellObj.line === 0 ? 'black' : 'white'
+    console.log(cellObj.cell);
+    console.log(cellObj.cell);
+    if (cellObj.cell === possibleCastle.left) changePieces('left', color)
+    if (cellObj.cell === possibleCastle.right) changePieces('right', color)
+
+    function changePieces(side, color) {
+        console.log('entrei');
+        var numberSide = side === 'left' ? 0 : 7
+        var abbrevColor = color === 'white' ? 'W' : 'B'
+        var objKing
+        var objRook
+
+        boardObj.forEach(line => {
+            line.forEach(obj => {
+                if (obj.piece.name === 'rook' && obj.column === numberSide && obj.piece.color === color) {
+                    objRook = obj
+                    boardObj[obj.line][obj.column].piece = false
+                } else if (obj.piece.name === 'king' && obj.piece.color === color) {
+                    objKing = obj
+                    boardObj[obj.line][obj.column].piece = false
+                }
+            })
+        });
+
+        console.log(objKing);
+        console.log(objRook);
+        var imgKing = deletePiece(objKing.cell.children)
+        var imgRook = deletePiece(objRook.cell.children)
+
+        console.log(imgKing);
+        console.log(imgRook);
+
+        if (numberSide === 0) {
+            boardObj[line][column].cell.appendChild(imgKing)
+            boardObj[line][column].piece = { name: 'king', color: color, firstPlay: true, src: `./../assets/pieces/king${abbrevColor}.png` }
+
+            boardObj[line][column + 1].cell.appendChild(imgRook)
+            boardObj[line][column + 1].piece = { name: 'rook', color: color, firstPlay: true, src: `./../assets/pieces/rook${abbrevColor}.png` }
+
+        }
+        else if (numberSide === 7) {
+            boardObj[line][column].cell.appendChild(imgKing)
+            boardObj[line][column].piece = { name: 'king', color: color, firstPlay: true, src: `./../assets/pieces/king${abbrevColor}.png` }
+
+            boardObj[line][column - 1].cell.appendChild(imgRook)
+            boardObj[line][column - 1].piece = { name: 'rook', color: color, firstPlay: true, src: `./../assets/pieces/rook${abbrevColor}.png` }
+
+        }
+
+        function deletePiece(childrens) {
+            for (let i = 0; i < childrens.length; i++) {
+                if (childrens[i].tagName.toLowerCase() === 'img') {
+                    let img = childrens[i]
+                    childrens[i].parentNode.removeChild(childrens[i])
+                    return img
+                }
+            }
+        }
+        refrash()
+    }
 }
