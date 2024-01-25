82ab7f73592e2c719d1e8fe92547f9c32da2201d
Luiz E. Carneiro <140028787+Luiz-E-Carneiro@users.noreply.github.com>
2024-01-24 23:00:32 -0300

    Validators Commit: Organization

---
+++ Chess/JS/logic/validator/check.js
@@ -0,0 +1,15 @@
+var objCheck = []
+var booleanCheck = false
+
+const verificCheck = () => {
+    if (!booleanCheck) return
+
+    var kingObj = objCheck[0].objKing
+
+    if (objCheck.length > 1) {
+        alert("KING MUST MOVE, or it's the end!!!")
+    } else {
+        console.log(objCheck);
+    }
+
+}
