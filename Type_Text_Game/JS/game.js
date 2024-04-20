8e1c3f8b15261dc75618de64a922682b205ca6ee
Luiz E. Carneiro <140028787+Luiz-E-Carneiro@users.noreply.github.com>
2024-04-19 23:03:21 -0300

    Full commit

---
+++ Type_Text_Game/JS/game.js
@@ -0,0 +1,137 @@
+const missesArea = document.getElementById('misses');
+const finishSound = new Audio('./assets/sounds/finish.mp3');
+let counting;
+let misses = 0;
+let started = false;
+let index = 0;
+let upperCase = false;
+let wrong = false;
+
+const alphabet = [
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ç',
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M',
+    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç',
+    'z', 'x', 'c', 'v', 'b', 'n', 'm',
+    "'", '-', ',', '.', ' '
+];
+
+const allLetters = document.getElementsByClassName('spanLetter');
+
+const startGame = () => {
+    allLetters[0].classList.add('selectedLetter');
+};
+
+const passLetter = () => {
+    allLetters[index - 1].classList.remove('selectedLetter');
+    allLetters[index - 1].classList.add('correctLetter');
+    if (index <= allLetters.length - 1) allLetters[index].classList.add('selectedLetter');
+    if (index === allLetters.length) {
+        finishGame();
+    }
+};
+
+const wrongLetter = () => {
+    miss();
+    if (index > 0) {
+        allLetters[index - 1].classList.remove('selectedLetter');
+    }
+    allLetters[index].classList.add('wrongLetter');
+    wrong = true;
+};
+
+const goBack = () => {
+    allLetters[index].classList.remove('wrongLetter');
+    wrong = false;
+};
+
+document.addEventListener('keydown', function (e) {
+    const key = e.key;
+    if (key === 'CapsLock') {
+        upperCase = !upperCase;
+    }
+    if (alphabet.includes(key) && !wrong) {
+        const keyClick = new Audio('./assets/sounds/click.mp3');
+        keyClick.playbackRate = 2.0;
+        keyClick.play();
+        verifyLetter(key);
+        // actionKeyboard(key);
+    }
+    if (key === 'Backspace') {
+        goBack();
+        let deleteSound = new Audio('./assets/sounds/delete.mp3');
+        deleteSound.play();
+        deleteSound.volume = 0.1;
+    }
+});
+
+const verifyLetter = (key) => {
+    const currentLetterText = allLetters[index].innerText;
+    let keyTyped = key;
+    if (allLetters[index].innerHTML === `&nbsp;` && key === " ") {
+        index++;
+        passLetter();
+        timer();
+    } else {
+        if (upperCase) {
+            keyTyped = keyTyped.toUpperCase();
+        }
+        if (currentLetterText === keyTyped) {
+            index++;
+            passLetter();
+        } else {
+            wrongLetter();
+        }
+        timer();
+    }
+};
+
+const finishGame = () => {
+    timer(true);
+    finishSound.play();
+};
+
+const miss = () => {
+    misses++
+    missesArea.innerText = `${misses < 10 ? "0" + misses : misses}`
+};
+
+const timer = (stop = false) => {
+    if (!started && !stop) {
+        started = true;
+        const timerElement = document.getElementById('timer');
+        let minutes = 0;
+        let seconds = 0;
+
+        counting = setInterval(() => {
+            if (seconds === 59) {
+                minutes++;
+                seconds = 0;
+            } else {
+                seconds++;
+            }
+            timerElement.innerText = `${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+        }, 1000);
+    }
+    if (stop) {
+        clearInterval(counting);
+    }
+};
+
+/*
+--> The following code is for the keyboard coding to be used on the mobile.
+const actionKeyboard = (key) => {
+    const keysDiv = document.getElementsByClassName('keyDiv');
+    for (let i = 0; i < keysDiv.length; i++) {
+        const currentLetter = keysDiv[i].innerText;
+        key = key.trim().toUpperCase();
+        if (key === currentLetter) {
+            keysDiv[i].classList.add('clicked');
+            setTimeout(() => {
+                keysDiv[i].classList.remove('clicked');
+            }, 200);
+        }
+    }
+};
+*/
---
+++ Type_Text_Game/JS/script.js
@@ -0,0 +1,101 @@
+const getText = async () => {
+    var text = ""
+    const res = await fetch("https://type.fit/api/quotes")
+    const data = await res.json()
+
+    var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    for (let i = 0; i < 5; i++) {
+        var rand = Math.floor(Math.random() * numbers.length)
+        numbers.splice(rand, 1)
+        text = text + " " + data[rand].text
+    }
+    var arrayParagaph = []
+    for (let i = 1; i < text.length; i++) {
+        var letter = text[i];
+        arrayParagaph.push(letter)
+    }
+    ready(arrayParagaph)
+
+}
+
+const ready = (array) => {
+    var paragraph = array
+    const textArea = document.getElementById('textArea')
+
+    var spanParagraph = document.createElement('span')
+    spanParagraph.classList.add('spanParagraph')            
+
+    var word = document.createElement('span')
+    word.classList.add('word')
+
+    for (let i = 0; i < paragraph.length; i++) {
+        let spanLetter = document.createElement('span')
+        spanLetter.classList.add('spanLetter')
+        if (paragraph[i] != " ") {
+            spanLetter.innerText = paragraph[i]
+            word.appendChild(spanLetter)
+            if (i === paragraph.length - 1) spanParagraph.appendChild(word)
+        } else {
+            spanLetter.innerHTML = '&nbsp;'
+            word.appendChild(spanLetter)
+            spanParagraph.appendChild(word)
+            word = document.createElement('span')
+            word.classList.add('word')
+        }
+        textArea.appendChild(spanParagraph)
+    }
+    startGame()
+}
+
+var btn = document.getElementById('restetBtn')
+btn.addEventListener('click', function () {
+    timer(true)
+    getText()
+    const textArea = document.getElementById('textArea')
+    textArea.innerHTML = ""
+    index = 0
+    started = false
+    wrong = false
+    misses = 0
+    const missesArea = document.getElementById('misses');
+    missesArea.innerText = "00"
+
+})
+getText()
+
+
+// This is the code to create a keyboard to let a smarphone play as well, but I won't implement it for the moment
+// const createKeyboard = () => {
+//     const keysArea = document.getElementById('areaKeyboard')
+//     var keyboard = [
+//         ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+//         ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Ç'],
+//         [',', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ".", 'Backspace'],
+//         ['Space']
+//     ]
+
+//     keyboard.forEach(array => {
+//         let lineKeys = document.createElement('div')
+//         lineKeys.classList.add('lineKeyboard')
+//         array.forEach(k => {
+//             let key = document.createElement('button')
+//             key.classList.add('keyDiv')
+//             if (k.length === 1) {
+//                 key.innerText = k
+//                 if (k === '.' || k === ',') key.classList.add('keyPunctuation')
+//             }
+//             else if (k === 'Backspace') {
+//                 key.innerHTML = `<span class="material-symbols-outlined">backspace</span>`
+//                 key.classList.toggle('keyBackSpace')
+//             } else {
+//                 key.classList.toggle('keySpace')
+//             }
+
+//             lineKeys.appendChild(key)
+//         });
+//         keysArea.appendChild(lineKeys)
+//     });
+
+// }
+
+// createKeyboard()
